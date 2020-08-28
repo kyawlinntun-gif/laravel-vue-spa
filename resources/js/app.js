bca270d62e39ea23626f53d9d6de4a9916775161
Kyaw Linn Tun <kyawlinntun100@gmail.com>
2020-08-28 13:13:11 +0630

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
